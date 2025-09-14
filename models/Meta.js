7b66f56c19976b8216787c8a81c5cbbdbb40376e
AndrehW27 <andrehwillian@hotmail.com>
2025-09-14 00:06:28 -0300

    cleaning comments

--- models/Meta.js
+++ models/Meta.js
@@ -18,18 +18,4 @@ const metaSchema = new mongoose.Schema({
 const Meta = mongoose.model("Meta", metaSchema);
 export default Meta;
 
-// const metaSchema = new mongoose.Schema({
-//     titulo: { type: String, required: true },
-//     descricao: { type: String },
-//     progresso: { type: Number, default: 0 },
-//     prazo: { type: String },
-//     prioridade: { type: String, enum: ["Baixa", "Média", "Alta"], default: "Média" },
-//     status: { type: String, enum: ["Novo", "Em andamento", "Pausado", "Concluído"], default: "Novo" },
-//     categoria: { type: String, enum: ["Outros", "Saúde", "Finanças", "Educação", "Pessoal", "Relacionamento", "Profissional", "Lazer", "Viagem", "Hobbie"], default: "Outros" },
-//     enviarLembrete: { type: Boolean, default: false },
-//     criarMiniMetas: { type: Boolean, default: false }
-// }, { timestamps: true });
-
-// export default mongoose.model("Meta", metaSchema);
-
 
