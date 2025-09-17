804b17fddb2cd994d88821caf40655ecd0d32ca7
AndrehW27 <andrehwillian@hotmail.com>
2025-09-17 01:12:17 -0300

    integracao cadastro usuario com front

--- routes/metaRoutes.js
+++ routes/metaRoutes.js
@@ -4,12 +4,14 @@ import {
   listarMetas,
   atualizarMeta,
   deletarMeta,
+  listarMetasPorUsuario,
 } from "../controllers/metaController.js";
 
 const router = express.Router();
 
 router.post("/", criarMeta);       // Criar meta
 router.get("/", listarMetas);      // Listar todas
+router.get("/:usuarioId", listarMetasPorUsuario);  // Listar metas de um usuário
 router.put("/:id", atualizarMeta); // Atualizar
 router.delete("/:id", deletarMeta); // Deletar
 
